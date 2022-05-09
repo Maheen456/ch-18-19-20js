9cd27ae6794decce6f1481a19f9c86a3529c91c1
Maheen456 <syedamaheenmajid90@gmail.com>
2022-05-09 05:39:11 +0500

    ch18-19-20

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+var courses=[];
+for(var i = 0; i < 5; i++){
+  courses.push(prompt("Enter courses: "));
+}
+alert(courses);
+for(var j=0;j<5;j++){
+newcourse1=prompt(courses[j]);
+}
+
+//save values with diff names and then splice them if required
